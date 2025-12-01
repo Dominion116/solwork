cb0c1beed14998b9213889d6e09edd6a03dbf5da
Dominion <limbotech116@gmail.com>
2025-12-01 14:47:07 +0100

    chore: add null-loader dependency to package.json

--- app/next.config.ts
+++ app/next.config.ts
@@ -15,8 +15,28 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'pbs.twimg.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+      },
     ],
+  },
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    // Exclude test files from being bundled
+    config.module.rules.push({
+      test: /node_modules\/.*\/test\/.*/,
+      use: 'null-loader',
+    });
+    return config;
   },
 };
 
 export default nextConfig;
+
